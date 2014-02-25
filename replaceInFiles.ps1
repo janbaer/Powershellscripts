42e9c7111a141e1243af3067778af1fb27d459e6
Jan Baer <jan@janbaer.de>
2014-02-25 10:10:46 +0100

    Update replaceInFiles.ps1

--- replaceInFiles.ps1
+++ replaceInFiles.ps1
@@ -1,16 +1,14 @@
 function replaceInFiles {
   param(
-    [string] $filePattern,
-    [string] $findWhat,
-    [string] $replaceWith
+    [Property (Mandatory=$true)][string] $filePattern,
+    [Property (Mandatory=$true)][string] $findWhat,
+    [Property (Mandatory=$true)][string] $replaceWith
   )
 
-  $files = dir -recurse -include $filePattern
+  $files = dir -recurse -include $filePattern | Select-String -SimpleMatch $findWhat | select -unique path | select -ExpandProperty path
 
   ForEach ($file in $files) {
     Write-Host ("Editing {0}"  -f $file) -ForegroundColor Yellow
-    (Get-Content $file) | % { $_ -replace $findWhat, $replaceWith } | Out-File -Encoding "UTF8" $file
+    (Get-Content ($file)) | % { $_ -replace $findWhat, $replaceWith } | Out-File -Encoding "UTF8" -FilePath $file
   }
-
   Write-Host ("Finished with {0} files" -f $files.Count) -ForegroundColor Green
-}
